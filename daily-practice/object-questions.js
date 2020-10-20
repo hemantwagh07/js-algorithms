c04d34dae2c139f934db7889c19584678feee8d1
Hemant Wagh <hemantwagh07@gmail.com>
2020-10-20 23:54:38 +0530

    Created it different obj questions
    Different object question

---
+++ daily-practice/object-questions.js
@@ -0,0 +1,9 @@
+var hero = {
+    _name: 'John Doe',
+    getSecretIdentity: function () {
+        return this._name;
+    }
+};
+var stoleSecretIdentity = hero.getSecretIdentity;
+console.log(stoleSecretIdentity());
+console.log(hero.getSecretIdentity());
