ea71914b8ec86628ebbb8a4bab62e1daf691f1cb
Hemant Wagh <hemantwagh07@gmail.com>
2020-11-10 22:01:34 +0530

    Sort array of object
    Sort array of objects using different properties

---
+++ daily-practice/SortObjectArray.js
@@ -0,0 +1,26 @@
+const person = [
+    { name: 'Hemant Wagh', age: 31 },
+    { name: 'Shiv Thakur', age: 30 },
+    { name: 'Rupesh Kohle', age: 35 },
+    { name: 'Sujit Singh', age: 21 },
+    { name: 'Farukh Shaikh', age: 41 }
+]
+
+function sortByLastName(obj) {
+    const compare = (a, b) => {
+        let [afname, alname] = a.name.split(' ');
+        let [bfname, blname] = b.name.split(' ');
+        return alname < blname ? -1 : alname < blname ? 1 : 0
+    }
+    obj.sort(compare);
+    return obj;
+}
+
+console.log(sortByLastName(person));
+
+function sortByAge(obj) {
+    obj.sort((a, b) => a.age - b.age);
+    return obj;
+}
+
+console.log(sortByAge(person));
