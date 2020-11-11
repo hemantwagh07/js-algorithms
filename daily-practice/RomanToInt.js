308facd4a572d79f92737058fbec8f95514ddc46
Hemant Wagh <hemantwagh07@gmail.com>
2020-11-11 12:24:32 +0530

    Convert roman numbers to Interger
    Convert roman numbers to Interger

---
+++ daily-practice/RomanToInt.js
@@ -0,0 +1,44 @@
+/*
+https://leetcode.com/problems/roman-to-integer/
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 
+12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. 
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. */
+
+
+var romanToInt = function (s) {
+    const romanVal = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+    let intVal = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (romanVal[s[i - 1]] < romanVal[s[i]]) {
+            intVal += romanVal[s[i]] - romanVal[s[i - 1]]; // When current element is > last element ex. IV, IX etc
+        } else {
+            intVal += (romanVal[s[i]] < romanVal[s[i + 1]]) ? 0 : romanVal[s[i]]; // Add zero when next element is < next element
+        }
+    }
+    return intVal;
+};
+
+console.log(romanToInt("III")); //3
+console.log(romanToInt("IV")); //4
+console.log(romanToInt("IX")); //9
+console.log(romanToInt("LVIII")); //58
+console.log(romanToInt("MCMXCIV")); //1994
